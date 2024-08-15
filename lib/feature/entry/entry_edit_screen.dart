3d37bb9ce48afb343c611eac732f9337666668fd
Nikolay Kantur <nikolay.kantur@gmail.com>
2024-08-15 06:22:47 +0300

    fix: EntryEditScreen now has 100% size

--- lib/feature/entry/entry_edit_screen.dart
+++ lib/feature/entry/entry_edit_screen.dart
@@ -12,6 +12,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
+import 'package:scaled_app/scaled_app.dart';
 
 class EntryEditScreen extends StatefulWidget {
   const EntryEditScreen({
@@ -120,136 +121,142 @@ class _EntryEditScreenState extends State<EntryEditScreen> {
     return Scaffold(
       backgroundColor: AppColor.white,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 25),
-              SizedBox(
-                height: 45,
-                child: Row(
-                  children: [
-                    const SizedBox(height: 25),
-                    Text(
-                      'Мои эмоции',
-                      style: AppStyle.mainPageTitle.copyWith(color: AppColor.green),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 24),
-              Text(
-                'Описание',
-                style: AppStyle.smallTitle.copyWith(color: AppColor.green),
-              ),
-              const SizedBox(height: 11),
-              TextField(
-                controller: controller,
-                minLines: 4,
-                maxLines: 6,
-                style: AppStyle.inputText.copyWith(color: AppColor.black),
-                decoration: InputDecoration(
-                  border: outlineInputBorder,
-                  focusedBorder: outlineInputBorder,
-                  errorBorder: outlineInputBorder,
-                  enabledBorder: outlineInputBorder,
-                ),
-              ),
-              const SizedBox(height: 24),
-              Text(
-                'Значок события',
-                style: AppStyle.smallTitle.copyWith(color: AppColor.green),
-              ),
-              const SizedBox(height: 11),
-              Wrap(
-                spacing: 25,
-                runSpacing: 18,
-                children: List.generate(
-                  Face.values.length,
-                  (index) {
-                    final face = Face.values[index];
-
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          selectedFace = face;
-                        });
-                      },
-                      behavior: HitTestBehavior.opaque,
-                      child: Opacity(
-                        opacity: selectedFace == face ? .2 : 1,
-                        child: SvgPicture.asset(
-                          face.icon,
-                          height: 40,
-                          width: 40,
-                        ),
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).scale().size.height -
+                MediaQuery.of(context).scale().padding.top -
+                MediaQuery.of(context).scale().padding.bottom,
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const SizedBox(height: 25),
+                SizedBox(
+                  height: 45,
+                  child: Row(
+                    children: [
+                      const SizedBox(height: 25),
+                      Text(
+                        'Мои эмоции',
+                        style: AppStyle.mainPageTitle.copyWith(color: AppColor.green),
                       ),
-                    );
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Text(
+                  'Описание',
+                  style: AppStyle.smallTitle.copyWith(color: AppColor.green),
+                ),
+                const SizedBox(height: 11),
+                TextField(
+                  controller: controller,
+                  minLines: 4,
+                  maxLines: 6,
+                  style: AppStyle.inputText.copyWith(color: AppColor.black),
+                  decoration: InputDecoration(
+                    border: outlineInputBorder,
+                    focusedBorder: outlineInputBorder,
+                    errorBorder: outlineInputBorder,
+                    enabledBorder: outlineInputBorder,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Text(
+                  'Значок события',
+                  style: AppStyle.smallTitle.copyWith(color: AppColor.green),
+                ),
+                const SizedBox(height: 11),
+                Wrap(
+                  spacing: 25,
+                  runSpacing: 18,
+                  children: List.generate(
+                    Face.values.length,
+                    (index) {
+                      final face = Face.values[index];
+
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedFace = face;
+                          });
+                        },
+                        behavior: HitTestBehavior.opaque,
+                        child: Opacity(
+                          opacity: selectedFace == face ? .2 : 1,
+                          child: SvgPicture.asset(
+                            face.icon,
+                            height: 40,
+                            width: 40,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 24),
+                AppButton(
+                  title: 'добавить фото+'.toUpperCase(),
+                  size: AppButtonSize.small,
+                  onTap: () async {
+                    final ImagePicker picker = ImagePicker();
+                    final XFile? tmp = await picker.pickImage(source: ImageSource.gallery);
+                    if (tmp == null) return;
+                    setState(() {
+                      image = tmp;
+                    });
                   },
                 ),
-              ),
-              const SizedBox(height: 58),
-              AppButton(
-                title: 'добавить фото+'.toUpperCase(),
-                size: AppButtonSize.small,
-                onTap: () async {
-                  final ImagePicker picker = ImagePicker();
-                  final XFile? tmp = await picker.pickImage(source: ImageSource.gallery);
-                  if (tmp == null) return;
-                  setState(() {
-                    image = tmp;
-                  });
-                },
-              ),
-              image == null
-                  ? const Spacer()
-                  : Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 25),
-                        child: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              image = null;
-                            });
-                          },
-                          child: Stack(
-                            alignment: Alignment.topRight,
-                            children: [
-                              Image.file(
-                                File(image!.path),
-                                fit: BoxFit.contain,
-                              ),
-                              Positioned(
-                                top: 10,
-                                right: 10,
-                                child: SvgPicture.asset(AppSvg.deleteSmall),
-                              ),
-                            ],
+                image == null
+                    ? const Spacer()
+                    : Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 25),
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                image = null;
+                              });
+                            },
+                            child: Stack(
+                              alignment: Alignment.topRight,
+                              children: [
+                                Image.file(
+                                  File(image!.path),
+                                  fit: BoxFit.contain,
+                                ),
+                                Positioned(
+                                  top: 10,
+                                  right: 10,
+                                  child: SvgPicture.asset(AppSvg.deleteSmall),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 49),
-                child: AppButton(
-                  title: 'сохранить'.toUpperCase(),
-                  onTap: () => _saveRecord(context),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 49),
+                  child: AppButton(
+                    title: 'сохранить'.toUpperCase(),
+                    onTap: () => _saveRecord(context),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 49),
-                child: AppButton(
-                  title: 'назад'.toUpperCase(),
-                  color: AppButtonColor.outline,
-                  onTap: () {
-                    Navigator.of(context).pop(true);
-                  },
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 49),
+                  child: AppButton(
+                    title: 'назад'.toUpperCase(),
+                    color: AppButtonColor.outline,
+                    onTap: () {
+                      Navigator.of(context).pop(true);
+                    },
+                  ),
                 ),
-              ),
-              const SizedBox(height: 39),
-            ],
+                const SizedBox(height: 39),
+              ],
+            ),
           ),
         ),
       ),
